bccedad9bee105bbbe6481eaeb8b9c86cec8bc4a
Parisa Delfani <parisa.delfani@snapp.cab>
2025-06-03 19:06:16 +0330

    Add favorite category usecase and repositiry into di modules

--- app/src/main/java/com/parinexus/newssample/di/RepositoryModule.kt
+++ app/src/main/java/com/parinexus/newssample/di/RepositoryModule.kt
@@ -1,15 +1,19 @@
 package com.parinexus.newssample.di
 
+import android.content.Context
 import com.parinexus.data.local.ArticleDao
 import com.parinexus.data.remote.ApiService
+import com.parinexus.data.repository.FavoriteCategoryRepositoryImpl
 import com.parinexus.data.repository.LocalRepositoryImpl
 import com.parinexus.data.repository.RemoteRepositoryImpl
+import com.parinexus.domain.repository.FavoriteCategoryRepository
 import com.parinexus.domain.repository.LocalRepository
 import com.parinexus.domain.repository.RemoteRepository
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -29,5 +33,11 @@ class RepositoryModule {
         return RemoteRepositoryImpl(apiService)
     }
 
-
+    @Provides
+    @Singleton
+    fun provideFavoriteCategoryRepository(
+        @ApplicationContext context: Context
+    ): FavoriteCategoryRepository {
+        return FavoriteCategoryRepositoryImpl(context)
+    }
 }
